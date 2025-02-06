13db7911dd17ddab77f12d85019e967d5e8c661f
Chavdar Dimitrov <chavdareto@gmail.com>
2025-02-06 17:25:44 +0200

    Update engine.rb due to error in rails 7

--- lib/rails_admin_tag_list/engine.rb
+++ lib/rails_admin_tag_list/engine.rb
@@ -1,8 +1,12 @@
+require_relative "../../app/helpers/rails_admin_tag_list/suggestions_helper"
+
 module RailsAdminTagList
   class Engine < ::Rails::Engine
-    initializer 'rails_admin_rag_list.action_controller' do |app|
+    initializer 'rails_admin_tag_list.action_controller' do |app|
       ActiveSupport.on_load :action_controller do
-        helper RailsAdminTagList::SuggestionsHelper
+        if self.respond_to?(:helper)
+          helper RailsAdminTagList::SuggestionsHelper
+        end
       end
     end
   end
